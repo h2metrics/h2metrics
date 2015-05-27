9779df014831c3e0c66094d35a779553408e4587
Martin <bauer.martin@univie.ac.at>
2015-05-27 15:27:15 +0200

    another setpath environment for matlab

---
+++ setpaths.m
@@ -0,0 +1,4 @@
+setenv('LD_LIBRARY_PATH','/users/bauerm/work/15_metrics_horeqnor/numerics/Ipopt/build/lib')
+setenv('LD_RUN_PATH', '/users/bauerm/work/15_metrics_horeqnor/numerics/Ipopt/build/lib')
+setenv('PATH', [getenv('PATH') ':/users/herman/COCONUT/bin:/users/bauerm/work/15_metrics_horeqnor/numerics/Ipopt/build/bin'])
+setenv('PATH', [getenv('PATH') ':/users/bauerm/Dropbox/H2_numerics/code/Martin'])
