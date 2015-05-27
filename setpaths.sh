79cfd30902d643940aeebcd142b8ccda36668961
Martin Bauer <bauerm@hadar.mat.univie.ac.at>
2015-05-27 15:22:00 +0200

    added path for lgbs

--- setpaths.sh
+++ setpaths.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export PATH=$PATH:/users/herman/COCONUT/bin:/users/bauerm/work/15_metrics_horeqnor/numerics/Ipopt/build/bin
+export PATH=$PATH:/users/herman/COCONUT/bin:/users/bauerm/work/15_metrics_horeqnor/numerics/Ipopt/build/bin:/users/bauerm/Dropbox/H2_numerics/code/Martin:
 export LD_LIBRARY_PATH=/users/bauerm/work/15_metrics_horeqnor/numerics/Ipopt/build/lib
 export LD_RUN_PATH=/users/bauerm/work/15_metrics_horeqnor/numerics/Ipopt/build/lib
