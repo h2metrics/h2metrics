a8129408deabd058c5740f29505f2761f11ba389
Jakob Møller-Andersen <jakmo@gmail.com>
2015-06-22 11:04:35 +0200

    Outcommented fastBSplineEval code, uses deBoor now

--- curveComposeDiff.m
+++ curveComposeDiff.m
@@ -33,10 +33,27 @@ phiPts = mod(phiPts, 2*pi);
 
 dNonper = [ d; d(1:nS,:) ];
 
-for ii = dSpace:-1:1
-    cPts(:,ii) = fastBSplineEval(knotsS, dNonper(:,ii), nS, phiPts);
-end
+%fastBsplineEval
+% for ii = dSpace:-1:1
+%     cPts(:,ii) = fastBSplineEval(knotsS, dNonper(:,ii), nS, phiPts);
+% end
+
+%deBoor
+cPts = deBoor( knotsS, nS, d1, phiPts,1,'periodic',true);
+
+% [ knots_dx,weights_dx,order_dx] = fastBSplineDer( knotsS,dNonper(:,1),nS );
+% [ knots_dx,weights_dy,order_dx] = fastBSplineDer( knotsS,dNonper(:,2),nS );
+% dcPts(:,1) = fastBSplineEval(knots_dx, weights_dx, nS-1, phiPts);
+% dcPts(:,2) = fastBSplineEval(knots_dx, weights_dy, nS-1, phiPts);
+
+% knots_dx_alt = knotsS(2:end-1);
+% weights_dx_alt = weights_dx(2:end-1);
+% weights_dy_alt = weights_dy(2:end-1);
+% dcPts_alt(:,1) = fastBSplineEval(knots_dx_alt, weights_dx_alt, nS-1, phiPts);
+% dcPts_alt(:,2) = fastBSplineEval(knots_dx_alt, weights_dy_alt, nS-1, phiPts);
 
 c = B_interpolS \ cPts;
+
+%[ knots_dx,weights_dx,order_dx] = fastBSplineDer( knotsS,dNonper(:,1),nS )
 
 end
