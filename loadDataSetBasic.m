9f18d6f974dab9bf99137db3982fb1be81850b27
Martins Bruveris <martins.bruveris@gmx.at>
2016-01-18 13:54:54 +0100

    Added propeller shapes to loadDataSetBasic.

--- loadDataSetBasic.m
+++ loadDataSetBasic.m
@@ -11,7 +11,8 @@ constSpeed = false; % Reparametrize to constant speed
 curveSet = {};
 returnCell = true;
 
-allCurves = {'H', 'O', 'T', 'U'};
+allCurves = {'H', 'O', 'T', 'U', 'prop0', 'prop3', 'prop4'};
+noise = 0;
 
 ii = 1;
 while ii <= length(varargin)
@@ -22,6 +23,9 @@ while ii <= length(varargin)
             case 'curves'
                 ii = ii + 1;
                 curveSet = varargin{ii};
+            case 'noise'
+                ii = ii + 1;
+                noise = varargin{ii};
             otherwise
                 error('Invalid option: ''%s''.',varargin{ii});
         end
@@ -41,6 +45,9 @@ for kk=length(curveSet):-1:1
     switch curveSet{kk}
         case {'H', 'O', 'T', 'U'}
             d0 = loadCurveLetters(curveSet{kk}, splineData, constSpeed);
+        case {'prop0', 'prop3', 'prop4'}
+            d0 = loadCurvePropeller(curveSet{kk}, noise, true, ...
+                                    splineData, constSpeed);
     end
     
     dList{kk} = d0;
@@ -152,6 +159,52 @@ len = 4*a*len; % Length of letter 'O'
 
 d = constructSplineApproximation(f, splineData);
 d = d / len * 2*pi;
+
+if constSpeed
+    d = curveReparamConstSpeed(d, splineData, splineData.quadData);
+end
+
+end
+
+function d = loadCurvePropeller(curveCode, noise, rescale, ...
+                                splineData, constSpeed)
+
+t = linspace(0, 2*pi, 400)';
+                            
+switch(curveCode)
+    case 'prop0'
+        c = [ cos(t) .* (1 + noise * sin(9*t)), ...
+              sin(t) .* (1 + noise * sin(9*t)) ];
+          
+    case 'prop3'
+        c = [ cos(t) .* (1 - 0.3*sin(3*t)), ...
+              sin(t) .* (1 - 0.3*sin(3*t)) ];
+        n = [ -cos(t).*(1-0.3*sin(3*t)) - sin(t).*(-0.3*cos(3*t)*3), ...
+              -sin(t).*(1-0.3*sin(3*t)) + cos(t).*(-0.3*cos(3*t)*3) ];
+        n = n ./ (sqrt(sum(n.^2, 2)) * [1 1]);
+        c = c - n .* ( noise*(sin(9*t))*[1 1] );
+    case 'prop4'
+        c = [ cos(t) .* (1 + 0.2*cos(4*t)), ...
+              sin(t) .* (1 + 0.2*cos(4*t)) ];
+        n = [ -cos(t).*(1+0.2*cos(4*t)) - sin(t).*(-0.2*sin(4*t)*4), ...
+              -sin(t).*(1+0.2*cos(4*t)) + cos(t).*(-0.2*sin(4*t)*4) ];
+        n = n ./ (sqrt(sum(n.^2, 2)) * [1 1]);
+        c = c - n .* ( noise*(cos(12*t))*[1 1] );
+end
+
+d = constructSplineApproximation(c, splineData);
+
+if rescale && abs(noise > eps)
+    dNoNoise = loadCurvePropeller( curveCode, 0, false, ...
+                                   splineData, constSpeed );
+    dLength = curveLength(dNoNoise, splineData, splineData.quadData);
+elseif rescale
+    dLength = curveLength(d, splineData, splineData.quadData);
+else
+    dLength = 2*pi;
+end
+
+d = d / dLength * 2*pi;
 
 if constSpeed
     d = curveReparamConstSpeed(d, splineData, splineData.quadData);
