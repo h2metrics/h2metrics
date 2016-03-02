9a0b34dc14c8819549e8095a2d90ed6b6bab92b5
Martins Bruveris <martins.bruveris@gmx.at>
2016-03-02 13:40:13 +0000

    Minor update.

--- journalRun.m
+++ journalRun.m
@@ -3,6 +3,7 @@ clear;
 
 dataDir = '~/diss/openprojects/h2_numerics/data/';
 prefixDir = '~/diss/openprojects/h2_numerics/journal/';
+plotDir = [prefixDir, 'final_plots/'];
 
 %% Create basic shapes
 journalBasic;
@@ -16,22 +17,23 @@ journalBasic;
 %% Fig. 4 - Convergence of BVP
 
 %% Fig. 5 - Continuity of distance function
-clearvars -except dataDir prefixDir
-
+clearvars -except dataDir prefixDir plotDir
 batch('journalNoise');   % Fig 5L
+
+clearvars -except dataDir prefixDir plotDir
 batch('journalNoise2');  % Fig 5R
 
 %% Fig. 6 - Symmetry
-clearvars -except dataDir prefixDir
-
+clearvars -except dataDir prefixDir plotDir
 batch('journalSymmetryDiff'); % Fig 6L, 6R
 
 %% Fig. 7 - Geodesic forward/backward
 
 %% Fig. 8 - Compatibiliy IVP/BVP
-clearvars -except dataDir prefixDir
-
+clearvars -except dataDir prefixDir plotDir
 batch('journalForward2');        % Fig 8L
+
+clearvars -except dataDir prefixDir plotDir
 batch('journalInitalVelocity');  % Fig 8R
 
 %% Fig. 9 - Karcher mean propellers
