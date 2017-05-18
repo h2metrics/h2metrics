59b13f6b3159845f697793248dc03fe6c8017b14
Martins Bruveris <martins.bruveris@gmx.at>
2017-05-18 14:26:04 +0100

    Added function to compute area inside curve.

---
+++ source/curveArea.m
@@ -0,0 +1,28 @@
+%% curveArea
+%
+% Calculates the area enclosed by a curve. We use Green's theorem to
+% calculate the are. This assumes that the curve is simply connected and
+% positively oriented. The formula is
+%   area(d) = 1/2 * int_0^{2*pi} x*y' - y*x' d\theta
+%
+% Input
+%   d
+%       Curve
+%   splineData
+%       Information about the splines used.
+%
+% Output
+%   A
+%       Area
+function A = curveArea( d, splineData )
+
+quadData = splineData.quadData;
+
+c = quadData.B_S * d;
+c_u = quadData.Bu_S * d;
+
+A = 0.5 * sum( (c(:,1) .* c_u(:,2) - c(:,2) .* c_u(:,1)) .* ...
+               quadData.quadWeightsS );
+
+end
+
