ee85f8674e8c8ebe8a682d5ff52b6264e1f27e0f
Martins Bruveris <martins.bruveris@gmx.at>
2018-03-29 17:11:44 -0400

    Added geodesicForward functionality for elastic and scale-invariant metrics.

--- source/metric/curveRiemH2InnerProd.m
+++ source/metric/curveRiemH2InnerProd.m
@@ -21,8 +21,10 @@
 %           [ L2, H1, H2, H1v, H1n ]
 %       We have the identity
 %           G = L2 + H1 + H2 + H1v + H1n
+%  dG
+%       Calculates the derivative of d --> G_d(v,w)
 %
-function [G,comp] = curveRiemH2InnerProd( d, v, w, splineData )
+function [G, comp, dG] = curveRiemH2InnerProd( d, v, w, splineData )
 
 %% Extract parameters
 a = splineData.a;
@@ -68,70 +70,117 @@ CspeedInv2 = CspeedInv .* CspeedInv;
 CspeedInv3 = CspeedInv .* CspeedInv2;
 CspeedInv5 = CspeedInv3 .* CspeedInv2;
 CspeedInv7 = CspeedInv5 .* CspeedInv2;
+CspeedInv9 = CspeedInv7 .* CspeedInv2;
 
-%% Building the metric
-Ct_L2 = VW .* Cspeed;
-Ct_H1 = VuWu .* CspeedInv;
-Ct_H1v = VuCu .*WuCu .*CspeedInv3;
-Ct_H1n = Ct_H1-Ct_H1v;
-Ct_H2 = VuWu .* CuCuu.^2 .* CspeedInv7 ...
-    - (VuWuu + VuuWu) .* CuCuu .* CspeedInv5 ...
-    + VuuWuu .* CspeedInv3;
-
+%% Adjust for scale-invariance
 if scaleInv
     ell = quadWeights' * Cspeed; %Length of c
     % Update coeffecients with the length weights
+    a_orig = a;
     a(1) = a(1)/ell^3;
     a(2) = a(2)/ell;
     a(3) = a(3)*ell;
     a(4) = a(4)/ell;
     a(5) = a(5)/ell;
 end
-energyIntegrand = a(1) * Ct_L2 + a(2) * Ct_H1 + a(3) * Ct_H2...
+
+%% Building the metric
+Ct_L2 = VW .* Cspeed;
+Ct_H1 = VuWu .* CspeedInv;
+Ct_H1v = VuCu .* WuCu .* CspeedInv3;
+Ct_H1n = Ct_H1-Ct_H1v;
+Ct_H2 = VuWu .* CuCuu.^2 .* CspeedInv7 ...
+    - (VuWuu + VuuWu) .* CuCuu .* CspeedInv5 ...
+    + VuuWuu .* CspeedInv3;
+
+energyIntegrand = a(1) * Ct_L2 + a(2) * Ct_H1 + a(3) * Ct_H2 ...
     + a(4) * Ct_H1v + a(5) * Ct_H1n;
 
 % Compute final energy
 G = quadWeights' * energyIntegrand;
-    if nargout >1
-        comp= [quadWeights' * a(1) * Ct_L2, quadWeights' * a(2) * Ct_H1,...
-            quadWeights' *a(3) * Ct_H2 ,quadWeights' *a(4) * Ct_H1v,...
-            quadWeights' *a(5) * Ct_H1n];
-    end    
+
+if nargout < 2
+    return
+end
+
+comp= [quadWeights' * a(1) * Ct_L2, quadWeights' * a(2) * Ct_H1,...
+       quadWeights' *a(3) * Ct_H2 ,quadWeights' *a(4) * Ct_H1v,...
+       quadWeights' *a(5) * Ct_H1n]; 
+
+%% Compute the gradient
+if nargout < 3
+    return
+end
+
+weight_L2 = CspeedInv .* VW .* quadWeights;
+sparseW_L2 = sparse(1:noQuadSites, 1:noQuadSites, weight_L2);
+
+weight_H1 = -CspeedInv3 .* VuWu .* quadWeights;
+sparseW_H1 = sparse(1:noQuadSites, 1:noQuadSites, weight_H1);
+
+weight_H1v_1 = CspeedInv3 .* VuCu .* quadWeights;
+weight_H1v_2 = CspeedInv3 .* WuCu .* quadWeights;
+weight_H1v_3 = -3 * CspeedInv5 .* VuCu .* WuCu .* quadWeights;
+
+sparseW_H1v_1 = sparse(1:noQuadSites, 1:noQuadSites, weight_H1v_1);
+sparseW_H1v_2 = sparse(1:noQuadSites, 1:noQuadSites, weight_H1v_2);
+sparseW_H1v_3 = sparse(1:noQuadSites, 1:noQuadSites, weight_H1v_3);
+
+weight_H2_1 = ( -7*CspeedInv9.*CuCuu2.*VuWu + ...
+                5*CspeedInv7.*CuCuu .* (VuWuu + VuuWu) + ...
+                -3*CspeedInv5.*VuuWuu ) .* quadWeights;
+weight_H2_2 = ( 2*CspeedInv7.*CuCuu.*VuWu + ...
+                -CspeedInv5.* (VuWuu + VuuWu) ) .* quadWeights;
+sparseW_H2_1 = sparse(1:noQuadSites, 1:noQuadSites, weight_H2_1);
+sparseW_H2_2 = sparse(1:noQuadSites, 1:noQuadSites, weight_H2_2);
+
+dG_L2 = zeros([N, dSpace]);
+dG_H1 = zeros([N, dSpace]);
+dG_H1v = zeros([N, dSpace]);
+dG_H1n = zeros([N, dSpace]);
+dG_H2 = zeros([N, dSpace]);
+
+for kk = dSpace:-1:1
+    dG_L2(:,kk) = Cu(:,kk)' * sparseW_L2 * quadData.Bu_S;
+    dG_H1(:,kk) = Cu(:,kk)' * sparseW_H1 * quadData.Bu_S;
+    dG_H1v(:,kk) = Wu(:,kk)' * sparseW_H1v_1 * quadData.Bu_S + ...
+                   Vu(:,kk)' * sparseW_H1v_2 * quadData.Bu_S + ...
+                   Cu(:,kk)' * sparseW_H1v_3 * quadData.Bu_S;
+    dG_H1n(:,kk) = dG_H1(:,kk) - dG_H1v(:,kk);
+    dG_H2(:,kk) = Cu(:,kk)' * sparseW_H2_1 * quadData.Bu_S + ...
+                    Cuu(:,kk)' * sparseW_H2_2 * quadData.Bu_S + ...
+                    Cu(:,kk)' * sparseW_H2_2 * quadData.Buu_S;
+end
+
+dG = a(1) * dG_L2 + a(2) * dG_H1 + a(3) * dG_H2 + ...
+        a(4) * dG_H1v + a(5) * dG_H1n;
+
+if scaleInv
+    % Derivatives of the coefficients with respect to length
+    ap = zeros([5, 1]);
+    if scaleInv
+        ap(1) = -3 * a_orig(1) / ell^4;
+        ap(2) = -a_orig(2) / ell^2;
+        ap(3) = a_orig(3);
+        ap(4) = -a_orig(4) / ell^2;
+        ap(5) = -a_orig(5) / ell^2;
+    end
+
+    energyIntegrand2 = ap(1) * Ct_L2 + ap(2) * Ct_H1 + ap(3) * Ct_H2 ...
+        + ap(4) * Ct_H1v + ap(5) * Ct_H1n;
+    G2 = quadWeights' * energyIntegrand2;
+
+    weight_ell = CspeedInv .* quadWeights;
+    sparseW_ell = sparse(1:noQuadSites, 1:noQuadSites, weight_ell);
+
+    dell = zeros([N, dSpace]);
+    for kk = dSpace:-1:1
+        dell(:,kk) = Cu(:,kk)' * sparseW_ell * quadData.Bu_S;
+    end
+    
+    dG = dG + G2 * dell;
 end
 
 
-
-%% Compute the gradient
-% if nargout < 2
-%     return
-% end
-% 
-% weight_L2 = CspeedInv .* VW .* quadWeights;
-% sparseW_L2 = sparse(1:noQuadSites, 1:noQuadSites, weight_L2);
-% 
-% weight_H1 = -CspeedInv3 .* VuWu .* quadWeights;
-% sparseW_H1 = sparse(1:noQuadSites, 1:noQuadSites, weight_H1);
-% 
-% weight_H2_1 = ( -7*CspeedInv9.*CuCuu2.*VuWu + ...
-%                 5*CspeedInv7.*CuCuu .* (VuWuu + VuuWu) + ...
-%                 -3*CspeedInv5.*VuuWuu ) .* quadWeights;
-% weight_H2_2 = ( 2*CspeedInv7.*CuCuu.*VuWu + ...
-%                 -CspeedInv5.* (VuWuu + VuuWu) ) .* quadWeights;
-% sparseW_H2_1 = sparse(1:noQuadSites, 1:noQuadSites, weight_H2_1);
-% sparseW_H2_2 = sparse(1:noQuadSites, 1:noQuadSites, weight_H2_2);
-% 
-% Gder_L2 = zeros([N, dSpace]);
-% Gder_H1 = zeros([N, dSpace]);
-% Gder_H2 = zeros([N, dSpace]);
-% 
-% for kk = dSpace:-1:1
-%     Gder_L2(:,kk) = a(1) * Cu(:,kk)' * sparseW_L2 * Bu;
-%     Gder_H1(:,kk) = a(2) * Cu(:,kk)' * sparseW_H1 * Bu;
-%     Gder_H2(:,kk) = a(2) * (Cu(:,kk)' * sparseW_H2_1 * Bu + ...
-%                             Cuu(:,kk)' * sparseW_H2_2 * Bu + ...
-%                             Cu(:,kk)' * sparseW_H2_2 * Buu);
-% end
-% 
-% Gder = Gder_L2 + Gder_H1 + Gder_H2;
 
 
--- source/riemgeom/geodesicForward.m
+++ source/riemgeom/geodesicForward.m
@@ -18,81 +18,42 @@
 %   q
 %       Discrete geodesic path
 %
-function q = geodesicForward(q0,q1,Nsteps,splineData,varargin)
+function q = geodesicForward(q0, q1, Nsteps, splineData, varargin)
 
-%TODO: Avoid using "full" command on B.
-a0 = splineData.a(1); %L2
-a1 = splineData.a(2); %H1
-a2 = splineData.a(3); %H2
+% Handle optional inputs
+p = inputParser;
+addParameter(p, 'endpoint', 0);
+parse(p, varargin{:});
 
-%Some code for handling optional inputs
-ii = 1;
-rule = 'left';
-endpoint = 0;
-while ii <= length(varargin)
-    if (isa(varargin{ii},'char'))
-         switch (lower(varargin{ii}))
-             case 'left' %Don't return gradient terms from end curves
-                 rule = 'left';
-             case 'mid'
-                 rule = 'mid';
-             case 'right'
-                 rule = 'right';
-             case 'endpoint'
-                 endpoint = 1;
-             otherwise
-                 error('Invalid option: ''%s''.',varargin{ii});
-         end     
-    end
-    ii = ii + 1;  
-end
+% Do we return only the endpoints
+endpoint = p.Results.endpoint;
 
-quadData = splineData.quadData;
+N = splineData.N;
+dSpace = splineData.dSpace;
 
-%Nsteps = 100;
-geodesicPoints = zeros(splineData.N,splineData.dSpace,Nsteps);
-exitFlags = zeros(1, Nsteps);
+% Here we save the geodesic
+geodesicPoints = zeros(N, dSpace, Nsteps);
 geodesicPoints(:,:,1) = q0;
 geodesicPoints(:,:,2) = q1;
 
-options_fsolve = optimset('TolFun',1e-6,'Display','off');
-%options_fsolve = optimoptions('fsolve');
-%options_fsolve = optimoptions(options_fsolve,'TolFun', 1e-6);
-%options_fsolve = optimoptions(options_fsolve,'Display','off');
-%options_fsolve = optimoptions(options_fsolve,'MaxIter',400);
-%options_fsolve = optimoptions(options_fsolve,'MaxFunEvals',10000);
+options = optimset('TolFun', 1e-6, ...
+                   'Display', 'off');
+%options = optimoptions('fsolve');
+%options = optimoptions(options,'TolFun', 1e-6);
+%options = optimoptions(options,'Display','off');
+%options = optimoptions(options,'MaxIter',400);
+%options = optimoptions(options,'MaxFunEvals',10000);
 
 for ii = 3:Nsteps
-
-%             if ~mod(ii/Nsteps*100,10)
-%                 disp(['i=',num2str(ii)]);
-%             end
+       
+        q_init = 2 * geodesicPoints(:,:,ii-1) - geodesicPoints(:,:,ii-2);
         
-        q_init = 2*geodesicPoints(:,:,ii-1) - geodesicPoints(:,:,ii-2);
-        
-        if strcmp(rule,'left')
-            F = @(q) LagragianLeftDer(q,geodesicPoints(:,:,ii-1),...
-                geodesicPoints(:,:,ii-2),quadData);
-        elseif strcmp(rule,'mid')
-            F = @(q) LagragianMidDer(q,geodesicPoints(:,:,ii-1),...
-                geodesicPoints(:,:,ii-2),quadData);
-        elseif strcmp(rule,'right')
-            F = @(q) LagragianLeftDer(q,geodesicPoints(:,:,ii-1),...
-                geodesicPoints(:,:,ii-2),quadData);
-        end
-        
-%         Fqinit = F(q_init);        
-%         disE = @(q)discreteH2Energy( {geodesicPoints(:,:,ii-2),q,q_init},splineData,quadData );        
-%         %clear dE_fd dE_nrm;
-%         %dE_fd = zeros(14,1);
-%         dE_nrm = zeros(14,1);
-%         for ll = 1:14
-% %             dE_fd(:,:,ll) = centralDiffJac( disE,geodesicPoints(:,:,ii-1),2*10^(-ll));
-%             dE_nrm(ll) = norm( Fqinit - centralDiffJac( disE,geodesicPoints(:,:,ii-1),2*10^(-ll)));
-%         end
-%         semilogy( dE_nrm )
-%         
-        [geodesicPoints(:,:,ii),fval,exitFlags(ii)] = fsolve( F, q_init, options_fsolve);
+        F = @(q) LagrangianLeftDer(q, ...
+                                   geodesicPoints(:,:,ii-1),...
+                                   geodesicPoints(:,:,ii-2), ...
+                                   splineData );
+          
+        [ geodesicPoints(:,:,ii), ~, ~ ] = fsolve( F, q_init, options);
 end
 
 q = geodesicPoints;
@@ -100,279 +61,15 @@ if endpoint
     q = geodesicPoints(:,:,end);
 end
 
-function [ Eder ] = LagragianLeftDer( d2,d1,d0,quadData )
-%Compute the derivative of the left-discrete energy of the 3 point path 
-%(q0,q1,q2) with respect to q1.
-% 
-
-%F = D_2(E_disc)
-%F(q2,h) =2*( 2*G_q0(q1-q0,h) - 2*G_q1(q2-q1,h) + D_(q1,h)G_*(q2-q1,q2-q1))
-%Solve F(q2,h) = 0, for all h = B_i*e_k.
-N = size(d0,1);
-Eder = zeros( N, 2);
-noQuadPointsS = quadData.noQuadPointsS;
-
-%Compute G(d1-d0,d1-d0)
-%W(q0,q1) = G_q0(q1-q0,q1-q0)
-
-d_v1 = d1 - d0;
-d_v2 = d2 - d1;
-
-B = (quadData.B_S);
-Bu = (quadData.Bu_S);
-Buu = (quadData.Buu_S);
-
-%C = quadData.B*Coefs;
-C0u = Bu*d0;
-C0uu = Buu*d0;
-C0speed = sum( C0u.^2 , 2).^(1/2);
-C0speedInv = 1./C0speed;
-C0speedInv2 = C0speedInv.^2;
-C0speedInv4 = C0speedInv2.^2;
-%C0speedInv6 = C0speedInv.^6;
-C0uC0uu = sum(C0u.*C0uu,2);
-
-C1u = Bu*d1;
-C1uu = Buu*d1;
-C1speed = sum( C1u.^2 , 2).^(1/2);
-C1speedInv = 1./C1speed;
-C1speedInv2 = C1speedInv.^2;
-C1speedInv3 = C1speedInv.*C1speedInv2;
-C1speedInv4 = C1speedInv2.^2;
-C1speedInv6 = C1speedInv3.^2;
-C1uC1uu = sum(C1u.*C1uu,2);
-
-V1 = B*d_v1;
-V1u = Bu*d_v1;
-V1uu = Buu*d_v1;
-
-V2 = B*d_v2;
-V2u = Bu*d_v2;
-V2uu = Buu*d_v2;
-
-%L2 Energy terms
-L2V2 = sum( V2.^2,2);
-
-%H1 terms, D_S V = 1/|c'|*V'
-H1V2 = C1speedInv2.*sum(V2u.^2,2);
-
-V2_C1Ds_X = C1speedInv.*V2u(:,1);
-V2_C1Ds_Y = C1speedInv.*V2u(:,2);
-
-%H2 Energy terms
-%D_S^2 V = DS2_1*V' + DS2_2*V'', for each curve
-C0Ds2_1 = -( C0uC0uu ).*C0speedInv4;
-C0Ds2_2 = C0speedInv2;
-C1Ds2_1 = -( C1uC1uu ).*C1speedInv4;
-C1Ds2_2 = C1speedInv2;
-
-V1_C0Ds2_X = C0Ds2_1.*V1u(:,1) + C0Ds2_2.*V1uu(:,1);
-V1_C0Ds2_Y = C0Ds2_1.*V1u(:,2) + C0Ds2_2.*V1uu(:,2);
-
-V2_C1Ds2_X = C1Ds2_1.*V2u(:,1) + C1Ds2_2.*V2uu(:,1);
-V2_C1Ds2_Y = C1Ds2_1.*V2u(:,2) + C1Ds2_2.*V2uu(:,2);
-
-H2V2 = V2_C1Ds2_X.^2 + V2_C1Ds2_Y.^2;
-
-%%% G_q0(v1,h)
-L2_V1h_1 = sparse(1:noQuadPointsS,1:noQuadPointsS,V1(:,1))*B;
-L2_V1h_2 = sparse(1:noQuadPointsS,1:noQuadPointsS,V1(:,2))*B;
-
-H1_V1h_1 = sparse(1:noQuadPointsS,1:noQuadPointsS,C0speedInv2.*V1u(:,1))*Bu;
-H1_V1h_2 = sparse(1:noQuadPointsS,1:noQuadPointsS,C0speedInv2.*V1u(:,2))*Bu;
-
-H2_V1h_1 = sparse(1:noQuadPointsS,1:noQuadPointsS,C0Ds2_1.*V1_C0Ds2_X)*Bu + ...
-    sparse(1:noQuadPointsS,1:noQuadPointsS,C0Ds2_2.*V1_C0Ds2_X)*Buu;
-H2_V1h_2 = sparse(1:noQuadPointsS,1:noQuadPointsS,C0Ds2_1.*V1_C0Ds2_Y)*Bu + ...
-    sparse(1:noQuadPointsS,1:noQuadPointsS,C0Ds2_2.*V1_C0Ds2_Y)*Buu;
-
-%%% G_q1(v2,h)
-L2_V2h_1 = sparse(1:noQuadPointsS,1:noQuadPointsS,V2(:,1))*B;
-L2_V2h_2 = sparse(1:noQuadPointsS,1:noQuadPointsS,V2(:,2))*B;
-
-H1_V2h_1 = sparse(1:noQuadPointsS,1:noQuadPointsS,V2_C1Ds_X.*C1speedInv)*Bu;
-H1_V2h_2 = sparse(1:noQuadPointsS,1:noQuadPointsS,V2_C1Ds_Y.*C1speedInv)*Bu;
-
-H2_V2h_1 = sparse(1:noQuadPointsS,1:noQuadPointsS,C1Ds2_1.*V2_C1Ds2_X)*Bu + ...
-    sparse(1:noQuadPointsS,1:noQuadPointsS,C1Ds2_2.*V2_C1Ds2_X)*Buu;
-H2_V2h_2 = sparse(1:noQuadPointsS,1:noQuadPointsS,C1Ds2_1.*V2_C1Ds2_Y)*Bu + ...
-    sparse(1:noQuadPointsS,1:noQuadPointsS,C1Ds2_2.*V2_C1Ds2_Y)*Buu;
-
-%%% D_(q1,h)G(v2,v2)
-C1speed_Var_1 = sparse(1:noQuadPointsS,1:noQuadPointsS,C1speedInv.*C1u(:,1))*Bu;
-C1speed_Var_2 = sparse(1:noQuadPointsS,1:noQuadPointsS,C1speedInv.*C1u(:,2))*Bu;
-
-V2_C1Ds_Var_X_1 = sparse(1:noQuadPointsS,1:noQuadPointsS,-V2_C1Ds_X.*C1speedInv3.*C1u(:,1).*V2u(:,1))*Bu;
-V2_C1Ds_Var_Y_1 = sparse(1:noQuadPointsS,1:noQuadPointsS,-V2_C1Ds_Y.*C1speedInv3.*C1u(:,1).*V2u(:,2))*Bu;
-V2_C1Ds_Var_X_2 = sparse(1:noQuadPointsS,1:noQuadPointsS,-V2_C1Ds_X.*C1speedInv3.*C1u(:,2).*V2u(:,1))*Bu;
-V2_C1Ds_Var_Y_2 = sparse(1:noQuadPointsS,1:noQuadPointsS,-V2_C1Ds_Y.*C1speedInv3.*C1u(:,2).*V2u(:,2))*Bu;
-
-V2_C1Ds2_Var_X_1 = sparse(1:noQuadPointsS,1:noQuadPointsS,V2_C1Ds2_X.*(4*C1speedInv6.*C1u(:,1).*C1uC1uu.*V2u(:,1)-C1speedInv4.*(C1uu(:,1).*V2u(:,1)+2*C1u(:,1).*V2uu(:,1))) )*Bu...
-    - sparse(1:noQuadPointsS,1:noQuadPointsS,V2_C1Ds2_X.*C1speedInv4.*C1u(:,1).*V2u(:,1))*Buu;
-V2_C1Ds2_Var_Y_1 = sparse(1:noQuadPointsS,1:noQuadPointsS,V2_C1Ds2_Y.*(4*C1speedInv6.*C1u(:,1).*C1uC1uu.*V2u(:,2)-C1speedInv4.*(C1uu(:,1).*V2u(:,2)+2*C1u(:,1).*V2uu(:,2))) )*Bu...
-    - sparse(1:noQuadPointsS,1:noQuadPointsS,V2_C1Ds2_Y.*C1speedInv4.*C1u(:,1).*V2u(:,2))*Buu;
-V2_C1Ds2_Var_X_2 = sparse(1:noQuadPointsS,1:noQuadPointsS,V2_C1Ds2_X.*(4*C1speedInv6.*C1u(:,2).*C1uC1uu.*V2u(:,1)-C1speedInv4.*(C1uu(:,2).*V2u(:,1)+2*C1u(:,2).*V2uu(:,1))) )*Bu...
-    - sparse(1:noQuadPointsS,1:noQuadPointsS,V2_C1Ds2_X.*C1speedInv4.*C1u(:,2).*V2u(:,1))*Buu;
-V2_C1Ds2_Var_Y_2 = sparse(1:noQuadPointsS,1:noQuadPointsS,V2_C1Ds2_Y.*(4*C1speedInv6.*C1u(:,2).*C1uC1uu.*V2u(:,2)-C1speedInv4.*(C1uu(:,2).*V2u(:,2)+2*C1u(:,2).*V2uu(:,2))) )*Bu...
-    - sparse(1:noQuadPointsS,1:noQuadPointsS,V2_C1Ds2_Y.*C1speedInv4.*C1u(:,2).*V2u(:,2))*Buu;
-
-    Eder_1_alt = 2*(...
-    (2*C0speed.*quadData.quadWeightsS)'*(a0*L2_V1h_1 + a1*H1_V1h_1+ a2*H2_V1h_1)...
-    - (2*C1speed.*quadData.quadWeightsS)'*(a0*L2_V2h_1 + a1*H1_V2h_1 + a2*H2_V2h_1)...
-    + ((a0*L2V2 +a1*H1V2 + a2*H2V2).*quadData.quadWeightsS)'*C1speed_Var_1...
-    + (2*C1speed.*quadData.quadWeightsS)'*( a1*(V2_C1Ds_Var_X_1 + V2_C1Ds_Var_Y_1)...
-    + a2*(V2_C1Ds2_Var_X_1+V2_C1Ds2_Var_Y_1)) );
-
-    Eder_2_alt = 2*(...
-    (2*C0speed.*quadData.quadWeightsS)'*(a0*L2_V1h_2 + a1*H1_V1h_2+ a2*H2_V1h_2)...
-    - (2*C1speed.*quadData.quadWeightsS)'*(a0*L2_V2h_2 + a1*H1_V2h_2 + a2*H2_V2h_2)...
-    + ((a0*L2V2 +a1*H1V2 + a2*H2V2).*quadData.quadWeightsS)'*C1speed_Var_2...
-    + (2*C1speed.*quadData.quadWeightsS)'*(a1*(V2_C1Ds_Var_X_2 + V2_C1Ds_Var_Y_2)...
-    + a2*(V2_C1Ds2_Var_X_2+V2_C1Ds2_Var_Y_2) ) );
-
-    Eder = [Eder_1_alt',Eder_2_alt'];
-    
 end
 
-function [ Eder ] = LagragianMidDer( d2,d1,d0,quadData )
-%Compute the derivative of the discrete energy of the 3 point path (q0,q1,q2)
-% with respect to q1.
-% 
-
-%E(q2,h) = 2*G_q0(q1-q0,h) + 2*G_q1(q2-q1,h) + D_(q1,h)G_*(q2-q1,q2-q1)
-%Solve F(q2,h) = 0, for all h = B_i*e_k.
-N = size(d0,1);
-Eder = zeros( N, 2);
-
-%Compute G_(d1-d0)/2 (d1-d0,d1-d0)
-%W(q0,q1) = G_q0(q1-q0,q1-q0)
-
-d_v1 = d1 - d0;
-d_v2 = d2 - d1;
-
-d1mid = (d1+d0)/2;
-d2mid = (d2+d1)/2;
-
-B = full(quadData.B_S);
-Bu = full(quadData.Bu_S);
-Buu = full(quadData.Buu_S);
-
-%C = quadData.B*Coefs;
-C0u = Bu*d1mid;
-C0uu = Buu*d1mid;
-C0speed = sum( C0u.^2 , 2).^(1/2);
-C0uC0uu = sum(C0u.*C0uu,2);
-
-C1u = Bu*d2mid;
-C1uu = Buu*d2mid;
-C1speed = sum( C1u.^2 , 2).^(1/2);
-C1uC1uu = sum(C1u.*C1uu,2);
-
-V1 = B*d_v1;
-V1u = Bu*d_v1;
-V1uu = Buu*d_v1;
-
-V2 = B*d_v2;
-V2u = Bu*d_v2;
-V2uu = Buu*d_v2;
-
-%L2 Energy terms
-L2V1 = sum( V1.^2,2);
-L2V2 = sum( V2.^2,2);
-
-%H2 Energy terms
-%D_S^2 V = DS2_1*V' + DS2_2*V'', for each curve
-C0Ds2_1 = -( sum(C0u.*C0uu,2) )./C0speed.^4;
-C0Ds2_2 = 1./C0speed.^2;
-C1Ds2_1 = -( sum(C1u.*C1uu,2) )./C1speed.^4;
-C1Ds2_2 = 1./C1speed.^2;
-
-V1_C0Ds2_X = C0Ds2_1.*V1u(:,1) + C0Ds2_2.*V1uu(:,1);
-V1_C0Ds2_Y = C0Ds2_1.*V1u(:,2) + C0Ds2_2.*V1uu(:,2);
-
-V2_C1Ds2_X = C1Ds2_1.*V2u(:,1) + C1Ds2_2.*V2uu(:,1);
-V2_C1Ds2_Y = C1Ds2_1.*V2u(:,2) + C1Ds2_2.*V2uu(:,2);
-
-H2V1 = V1_C0Ds2_X.^2 + V1_C0Ds2_Y.^2;
-H2V2 = V2_C1Ds2_X.^2 + V2_C1Ds2_Y.^2;
-
-%
-for kk = N:-1:1
-    %%% G_q1mid(v1,h)
-    L2_V1h(:,kk,1) = V1(:,1).*B(:,kk);
-    L2_V1h(:,kk,2) = V1(:,2).*B(:,kk);
-    
-    h_C0DS2 = C0Ds2_1.*Bu(:,kk) + C0Ds2_2.*Buu(:,kk);
-    H2_V1h(:,kk,1) = V1_C0Ds2_X.*h_C0DS2;
-    H2_V1h(:,kk,2) = V1_C0Ds2_Y.*h_C0DS2;
-    
-    %%% G_q2mid(v2,h)
-    L2_V2h(:,kk,1) = V2(:,1).*B(:,kk);
-    L2_V2h(:,kk,2) = V2(:,2).*B(:,kk);
-    
-    h_C1DS2 = C1Ds2_1.*Bu(:,kk) + C1Ds2_2.*Buu(:,kk);
-    H2_V2h(:,kk,1) = V2_C1Ds2_X.*h_C1DS2;
-    H2_V2h(:,kk,2) = V2_C1Ds2_Y.*h_C1DS2; 
-    
-    %%% D_(q1mid,h/2)G(v1,v1)
-    C0speed_Var(:,kk,1) = 1./C0speed.*C0u(:,1).*Bu(:,kk);
-    C0speed_Var(:,kk,2) = 1./C0speed.*C0u(:,2).*Bu(:,kk);
-    
-    V1_C0Ds2_X_Var(:,kk,1) = 4./C0speed.^6.*C0u(:,1).*Bu(:,kk).*C0uC0uu.*V1u(:,1) - ...
-        1./C0speed.^4.*( (C0uu(:,1).*Bu(:,kk) + C0u(:,1).*Buu(:,kk)).*V1u(:,1) ...
-        + 2*C0u(:,1).*Bu(:,kk).*V1uu(:,1) );
-    V1_C0Ds2_Y_Var(:,kk,1) = 4./C0speed.^6.*C0u(:,1).*Bu(:,kk).*C0uC0uu.*V1u(:,2) - ...
-        1./C0speed.^4.*( (C0uu(:,1).*Bu(:,kk) + C0u(:,1).*Buu(:,kk)).*V1u(:,2) ...
-        + 2*C0u(:,1).*Bu(:,kk).*V1uu(:,2) );
-    
-    V1_C0Ds2_X_Var(:,kk,2) = 4./C0speed.^6.*C0u(:,2).*Bu(:,kk).*C0uC0uu.*V1u(:,1) - ...
-        1./C0speed.^4.*( (C0uu(:,2).*Bu(:,kk) + C0u(:,2).*Buu(:,kk)).*V1u(:,1) ...
-        + 2*C0u(:,2).*Bu(:,kk).*V1uu(:,1) );
-    V1_C0Ds2_Y_Var(:,kk,2) = 4./C0speed.^6.*C0u(:,2).*Bu(:,kk).*C0uC0uu.*V1u(:,2) - ...
-        1./C0speed.^4.*( (C0uu(:,2).*Bu(:,kk) + C0u(:,2).*Buu(:,kk)).*V1u(:,2) ...
-        + 2*C0u(:,2).*Bu(:,kk).*V1uu(:,2) );
-    
-    
-    %%% D_(q2mid,h/2)G(v2,v2)
-    C1speed_Var(:,kk,1) = 1./C1speed.*C1u(:,1).*Bu(:,kk);
-    C1speed_Var(:,kk,2) = 1./C1speed.*C1u(:,2).*Bu(:,kk);
-    
-    V2_C1Ds2_X_Var(:,kk,1) = 4./C1speed.^6.*C1u(:,1).*Bu(:,kk).*C1uC1uu.*V2u(:,1) - ...
-        1./C1speed.^4.*( (C1uu(:,1).*Bu(:,kk) + C1u(:,1).*Buu(:,kk)).*V2u(:,1) ...
-        + 2*C1u(:,1).*Bu(:,kk).*V2uu(:,1) );
-    V2_C1Ds2_Y_Var(:,kk,1) = 4./C1speed.^6.*C1u(:,1).*Bu(:,kk).*C1uC1uu.*V2u(:,2) - ...
-        1./C1speed.^4.*( (C1uu(:,1).*Bu(:,kk) + C1u(:,1).*Buu(:,kk)).*V2u(:,2) ...
-        + 2*C1u(:,1).*Bu(:,kk).*V2uu(:,2) );
-    
-    V2_C1Ds2_X_Var(:,kk,2) = 4./C1speed.^6.*C1u(:,2).*Bu(:,kk).*C1uC1uu.*V2u(:,1) - ...
-        1./C1speed.^4.*( (C1uu(:,2).*Bu(:,kk) + C1u(:,2).*Buu(:,kk)).*V2u(:,1) ...
-        + 2*C1u(:,2).*Bu(:,kk).*V2uu(:,1) );
-    V2_C1Ds2_Y_Var(:,kk,2) = 4./C1speed.^6.*C1u(:,2).*Bu(:,kk).*C1uC1uu.*V2u(:,2) - ...
-        1./C1speed.^4.*( (C1uu(:,2).*Bu(:,kk) + C1u(:,2).*Buu(:,kk)).*V2u(:,2) ...
-        + 2*C1u(:,2).*Bu(:,kk).*V2uu(:,2) );
-    
-    %%% Compute F(q2)
-    Eder(kk,1) = 2*sum( ( 2*C0speed.*(L2_V1h(:,kk,1) + H2_V1h(:,kk,1)) ...
-    - 2*C1speed.*(L2_V2h(:,kk,1) + H2_V2h(:,kk,1)) ...
-    + 1/2*C0speed_Var(:,kk,1).*(L2V1 + H2V1) ...
-    + C0speed.*( V1_C0Ds2_X.*V1_C0Ds2_X_Var(:,kk,1) + V1_C0Ds2_Y.*V1_C0Ds2_Y_Var(:,kk,1) ) ...
-    + 1/2*C1speed_Var(:,kk,1).*(L2V2 + H2V2) ...
-    + C1speed.*( V2_C1Ds2_X.*V2_C1Ds2_X_Var(:,kk,1) + V2_C1Ds2_Y.*V2_C1Ds2_Y_Var(:,kk,1) )...
-    ).*quadData.quadWeightsS) ;
-    
-    Eder(kk,2) = 2*sum( ( 2*C0speed.*(L2_V1h(:,kk,2) + H2_V1h(:,kk,2)) ...
-    - 2*C1speed.*(L2_V2h(:,kk,2) + H2_V2h(:,kk,2)) ...
-    + 1/2*C0speed_Var(:,kk,2).*(L2V1 + H2V1) ...
-    + C0speed.*( V1_C0Ds2_X.*V1_C0Ds2_X_Var(:,kk,2) + V1_C0Ds2_Y.*V1_C0Ds2_Y_Var(:,kk,2) ) ...
-    + 1/2*C1speed_Var(:,kk,2).*(L2V2 + H2V2) ...
-    + C1speed.*( V2_C1Ds2_X.*V2_C1Ds2_X_Var(:,kk,2) + V2_C1Ds2_Y.*V2_C1Ds2_Y_Var(:,kk,2) )...
-    ).*quadData.quadWeightsS) ;
+% E(d0, d1, d2) = G_{d0}(d1-d0, d1-d0) + G_{d1}(d2-d1, d2-d2)
+% D_{d1} E(...) (h) = 2 * G_{d0}(d1-d0, h) - 2 * G_{d1}(d2-d1, h)
+%                      + D_{d1} G_{}(d2-d1,d2-d1) (h)
+function Eder = LagrangianLeftDer(d2, d1, d0, splineData)
+   [~, ~, dG] = curveRiemH2InnerProd(d1, d2-d1, d2-d1, splineData);
+   
+   Eder = 2 * curveRiemH2Flat(d0, d1-d0, splineData) ...
+            - 2 * curveRiemH2Flat(d1, d2-d1, splineData) ...
+            + dG;
 end
-
-end
-
-
-
-
-end
-
