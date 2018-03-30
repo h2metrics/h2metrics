2950cab59408419793e09e8b44a66455039e7ae9
Martin Bauer <bauer@wg-dhcp201d200d221.secure.wireless.fsu.edu>
2018-03-30 16:13:07 -0400

    changed example

--- examples/example.m
+++ examples/example.m
@@ -3,14 +3,14 @@
 %% Precaution
 clear all;
 %% Set paths
-% This should be done in a setup script
 addpath(genpath('../source'));
 addpath('../lib/varifolds');
 addpath('../lib/hanso2_2');
 addpath('../lib/export_fig');
 %% Setup parameters for curve discretization
 splineData = constructSplineData;
-splineData.N = 40; % Number of spline controll points for spatial discretization 
+splineData.N = 40; % Number of spline controll points for spatial discretization
+splineData.nS = 2; % Quadratic splines are needed for second order metrics  
 splineData.Nt = 5; % Number of spline controll points for time discretization 
 splineData.curveClosed=1; %Closed Curves (set to 0 for open curves)
 splineData = finishSetup(splineData);
@@ -20,16 +20,19 @@ splineData.varData.kernelSizeGrass = 0.3;
 splineData.varData.pts = [];
 splineData = finishSetup( splineData );
 %% Load some curves
-c0 = importdata('OAS1_0003.txt');
-c1 = importdata('OAS1_0016.txt');
+c0 = importdata('./data/OAS1_0016.txt');
+c1 = importdata('./data/OAS1_0022.txt');
 %Construct spline approximation
 d0= constructSplineApproximation(c0(2:end,:),splineData);
 d1= constructSplineApproximation(c1(2:end,:),splineData);
 %Rescale curves to length 2pi (not necessary)
 d0 = 2*pi* d0/curveLength(d0,splineData);
 d1 = 2*pi*d1/curveLength(d1,splineData);
-%Plot the data
-plotCurve({d0,d1}, splineData) 
+dList = rigidAlignmentVarifold({d0,d1},splineData);%Prealign the data
+d0=dList{1};
+d1=dList{2};
+plotCurve({d0,d1}, splineData) %Plot the data
+
 
 %% Calculate Geodesic between c0 and c1
 splineData.a=[.1 1 0 0 0]; %Constants for the metric:
@@ -39,7 +42,7 @@ splineData.options.optScal=0; %Minimize over scalings of the target curve
 splineData.options.optRot=1; %Minimize over rotations of the target curve
 splineData.options.optTra=1; %Minimize over translations of the target curve
 splineData.options.useAugmentedLagrangian = false; %Use quadratic penalty term 
-splineData.options.varLambda = 10; %Weight of the similarity measure
+splineData.options.varLambda = 100; %Weight of the similarity measure
 %%
 tic
 [optE, optPath, optGa, ~] = geodesicBvp(d0, d1, ...
@@ -51,5 +54,4 @@ disp((optE)^(1/2));
 %% Plot of the minimizing geodesic. The target curve is ploted in red.. 
 clf
 plotPath2(optPath,d2,splineData)
-%%
 
